9a32822bf1c6f7b743c007dd87684140f0eaa83e
dandmwg <dandmwg@gmail.com>
2025-06-01 17:53:57 +0100

    Add Stripe Zakat donation API route

---
+++ api/create-checkout-session.js
@@ -0,0 +1,37 @@
+// /api/create-checkout-session.js
+
+import Stripe from 'stripe';
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).send('Method Not Allowed');
+  }
+
+  try {
+    const { amount } = req.body;
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items: [{
+        price_data: {
+          currency: 'usd',
+          product_data: {
+            name: 'Zakat Donation',
+          },
+          unit_amount: amount, // Amount in cents
+        },
+        quantity: 1,
+      }],
+      mode: 'payment',
+      success_url: `${req.headers.origin}/?success=true`,
+      cancel_url: `${req.headers.origin}/?canceled=true`,
+    });
+
+    return res.status(200).json({ url: session.url });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Something went wrong.' });
+  }
+}
